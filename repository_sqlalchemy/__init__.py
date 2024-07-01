5ce84dd680be7fe8ce74c501bc5d1547012d5142
Ryan Zheng <nogrethumphrey@gmail.com>
2024-07-01 07:00:26 +0200

    fix the find package in setup file

--- repository_sqlalchemy/__init__.py
+++ repository_sqlalchemy/__init__.py
@@ -6,4 +6,6 @@ __all__ = [
     "transaction",
     "transactional",
     "Base",
+    "DatabaseConfig",
+    "DatabaseEngineFactory",
 ]
--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="repository_sqlalchemy",
     version="0.1.0",
-    packages=find_packages(where="repository_sqlalchemy"),
-    package_dir={"": "repository_sqlalchemy"},
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "sqlalchemy>=2.0.31",
-        "fastapi>=0.111.0",
     ],
     extras_require={
         "dev": [
@@ -15,13 +14,13 @@ setup(
             "pytest-asyncio>=0.23.7",
         ],
     },
-    python_requires=">=3.10",
+    python_requires=">=3.8",
     author="Ryan Zheng",
     author_email="ryan.zheng.work@gmail.com",
     description="A small library to simplify SQLAlchemy usage with FastAPI",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/repository_sqlalchemy",
+    url="https://github.com/ryan-zheng-teki/repository_sqlalchemy",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
